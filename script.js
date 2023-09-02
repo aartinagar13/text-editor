3fda23ad8d8910766d52bdaa01ce88f0099001ed
Aarti Nagar <101711456+aartinagar13@users.noreply.github.com>
2023-09-03 00:34:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+ function run()
+{
+    var code = document.getElementById("code").value;
+    var result_box = document.getElementById("result");
+    result_box.innerHTML = code;
+    var code_box = document.getElementById("code_box");
+    code_box.style.display ="none";
+    var result_box = document.getElementById("result_box");
+    result_box.style.display = "block";
+    var btn = document.getElementById("run_btn");
+    btn.style.display = "none";
+    var b_btn = document.getElementById("icon");
+    b_btn.style.display = "block";
+}
+function back()
+{
+    var result_box = document.getElementById("result_box");
+    result_box.style.display = "none";
+    var code_box = document.getElementById("code_box");
+    code_box.style.display ="block";
+    var btn = document.getElementById("run_btn");
+    btn.style.display = "block";
+    var b_btn = document.getElementById("icon");
+    b_btn.style.display = "none";
+}
